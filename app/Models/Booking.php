95edd4abf0e24f582b65db055be131dc380aac8a
Pashutaz <pazaytsev@gmail.com>
2021-04-03 19:36:51 +0300

    Add bookings

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -5,12 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Bookable extends Model
+class Booking extends Model
 {
     use HasFactory;
 
-    public function bookings()
+    protected $fillable = ['from', 'to'];
+
+    public function bookable()
     {
-        return $this->hasMany(Booking::class);
+        return $this->belongsTo(Bookable::class);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(BookablesTableSeeder::class);
+        // $this->call(BookablesTableSeeder::class);
+        $this->call(BookingsTableSeeder::class);
     }
 }
