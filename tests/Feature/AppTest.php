517b6dc94d123ab603e49a1eecf38ae961c1920a
Pashutaz <pazaytsev@gmail.com>
2022-09-18 22:43:32 +0300

    Update tests

--- tests/Feature/AppTest.php
+++ tests/Feature/AppTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class AppTest extends TestCase
 {
     /**
      * A basic test example.
@@ -17,5 +17,13 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function testBookablePrice()
+    {
+        $response = $this->get('/api/bookables/32/price?from=2021-04-04&to=2021-04-07');
+
+        $response->assertStatus(200);
+        $this->assertArrayHasKey('data', $response->json());
     }
 }
