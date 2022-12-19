0bf53c58dfea7c2cba472c33a313dab86f79677f
Pashutaz <pazaytsev@gmail.com>
2022-12-19 22:16:47 +0400

    Fix checkout phone validation

--- app/Http/Controllers/Api/CheckoutController.php
+++ app/Http/Controllers/Api/CheckoutController.php
@@ -11,23 +11,23 @@ class CheckoutController extends Controller
     /**
      * Handle the shopping cart checkout request
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  Request  $request
+     * @return Response
      */
     public function __invoke(Request $request): Response
     {
         $userData = $request->validate([
             'email'   => 'required|email',
             'name'    => 'required|string',
-            'phone'   => 'required|numeric|min:5|max:11',
-            'country'  => 'min:2',
+            'phone'   => 'required|string|min:5|max:11',
+            'country' => 'min:2',
         ]);
-        $bookingsData = $request->validate([
+        $bookingsData = current($request->validate([
             'bookings'               => 'required|array|min:1',
             'bookings.*.bookable_id' => 'required|numeric',
             'bookings.*.from'        => 'required|date|after:today',
             'bookings.*.to'          => 'required|date|after:bookings.*.from',
-        ])['bookings'];
+        ]));
 
         $bookings = collect($bookingsData)->map(function(array $item) {
             $from = $item['from'];
@@ -40,7 +40,7 @@ class CheckoutController extends Controller
                 return Booking::create([
                     'from'        => $from,
                     'to'          => $to,
-                    'bookable_id' => $bookable->id,
+                    'bookable_id' => $bookable->getKey(),
                     'cost'        => $totalCost
                 ]);
             }
